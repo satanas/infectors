5d44347b5f649256c6c97e0f22307b3d7bf718ca
satanas <wil.alejandro@gmail.com>
2015-02-22 15:34:41 -0300

    Added virus animation

--- virus.js
+++ virus.js
@@ -11,6 +11,15 @@ var Virus = function(x, y, type) {
   } else if (type === colorVariant.GREEN) {
     this.frame = 2;
   }
+
+  if (type === colorVariant.RED) {
+    this.animations.add('main', [0, 3, 6, 9], 12, true);
+  } else if (type === colorVariant.BLUE) {
+    this.animations.add('main', [1, 4, 7, 10], 12, true);
+  } else if (type === colorVariant.GREEN) {
+    this.animations.add('main', [2, 5, 8, 11], 12, true);
+  }
+  this.animations.play('main');
   groups.viruses.add(this);
 };
 
