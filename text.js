0743d7dd962983c8a8a7ee73c4caa041cc87794f
satanas <wil.alejandro@gmail.com>
2015-02-28 20:02:27 -0300

    Returned value on bitmapTextCentered

--- text.js
+++ text.js
@@ -4,4 +4,5 @@ function bitmapTextCentered(y, font, text, size) {
   var label = game.add.bitmapText(0, y, font, text, size);
   label.updateTransform();
   label.x = (game.width - label.width) / 2;
+  return label;
 }
