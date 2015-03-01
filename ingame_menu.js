73ad65030a4b2d8bb9208a2162012965f17e9628
satanas <wil.alejandro@gmail.com>
2015-03-01 00:41:25 -0300

    Implemented new object for ingame menu

---
+++ ingame_menu.js
@@ -0,0 +1,74 @@
+'use strict';
+
+var IngameMenu = function(play) {
+  this.play = play;
+  this.option = 0;
+
+  this.pauseKeyUp = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+  this.pauseKeyDown = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
+  this.pauseKeyEnter = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
+};
+
+IngameMenu.prototype.constructor = IngameMenu;
+
+IngameMenu.prototype.update = function() {
+  if (this.option === 0) {
+      this.cursor.y = 165;
+  } else if (this.option === 1) {
+      this.cursor.y = 225;
+  } else if (this.option === 2) {
+      this.cursor.y = 285;
+  }
+};
+
+IngameMenu.prototype.show = function() {
+  this.option = 0;
+
+  this.mask = game.add.graphics(game.camera.x, game.camera.y);
+  this.mask.beginFill(0x000000, 1);
+  this.mask.drawRect(game.camera.x, game.camera.y, game.width, game.height);
+  this.mask.alpha = 0.5;
+  this.mask.endFill();
+
+  this.menuBg = game.add.sprite(195, 100, 'menu');
+  this.cursor = game.add.sprite(243, 165, 'cursor');
+  this.continueLabel = bitmapTextCentered(160, 'engeexpa', 'Continue', 30);
+  this.restartLabel = bitmapTextCentered(220, 'engeexpa', 'Restart', 30);
+  this.quitLabel = bitmapTextCentered(280, 'engeexpa', 'Quit', 30);
+
+  this.pauseKeyUp.onDown.add(this.moveCursorUp, this);
+  this.pauseKeyDown.onDown.add(this.moveCursorDown, this);
+  this.pauseKeyEnter.onDown.add(this.executeMenuOption, this);
+};
+
+IngameMenu.prototype.hide = function() {
+  this.mask.destroy();
+  this.menuBg.destroy();
+  this.continueLabel.destroy();
+  this.restartLabel.destroy();
+  this.quitLabel.destroy();
+  this.cursor.destroy();
+
+  this.pauseKeyUp.onDown.removeAll();
+  this.pauseKeyDown.onDown.removeAll();
+  this.pauseKeyEnter.onDown.removeAll();
+};
+
+IngameMenu.prototype.moveCursorDown = function() {
+    this.option += 1;
+    if (this.option > 2) this.option = 0;
+  };
+
+IngameMenu.prototype.moveCursorUp = function() {
+    this.option -= 1;
+    if (this.option < 0) this.option = 2;
+  };
+
+IngameMenu.prototype.executeMenuOption = function() {
+  if (this.option === 1) {
+    game.state.start('play');
+  } else if (this.option === 2) {
+    game.state.start('menu');
+  }
+  this.play.togglePause();
+}
--- play.js
+++ play.js
@@ -58,11 +58,9 @@ var playState = {
 
     //groups.walls.debug = true;
     this.pausedGame = false;
-    this.pauseKey = game.input.keyboard.addKey(Phaser.Keyboard.P);
+    this.pauseKey = game.input.keyboard.addKey(Phaser.Keyboard.ESC);
     this.pauseKey.onUp.add(this.togglePause, this);
-    this.pauseKeyUp = game.input.keyboard.addKey(Phaser.Keyboard.UP);
-    this.pauseKeyDown = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
-    this.pauseKeyEnter = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
+    this.ingameMenu = new IngameMenu(this);
   },
 
   update: function() {
@@ -73,76 +71,18 @@ var playState = {
   },
 
   pauseUpdate: function() {
-    if (this.menuOption === 0) {
-        this.cursor.y = 165;
-    } else if (this.menuOption === 1) {
-        this.cursor.y = 225;
-    } else if (this.menuOption === 2) {
-        this.cursor.y = 285;
-    }
+    this.ingameMenu.update();
   },
 
   togglePause: function() {
     this.pausedGame = !this.pausedGame;
     if (this.pausedGame) {
-      this.showPauseMenu();
+      this.ingameMenu.show();
     } else {
-      this.quitPauseMenu();
+      this.ingameMenu.hide();
     }
     game.paused = this.pausedGame;
   },
-
-  showPauseMenu: function() {
-    this.menuOption = 0;
-
-    this.mask = game.add.graphics(game.camera.x, game.camera.y);
-    this.mask.beginFill(0x000000, 1);
-    this.mask.drawRect(game.camera.x, game.camera.y, game.width, game.height);
-    this.mask.alpha = 0.5;
-    this.mask.endFill();
-
-    this.menuBg = game.add.sprite(195, 100, 'menu');
-    this.cursor = game.add.sprite(243, 165, 'cursor');
-    this.continueLabel = bitmapTextCentered(160, 'engeexpa', 'Continue', 30);
-    this.restartLabel = bitmapTextCentered(220, 'engeexpa', 'Restart', 30);
-    this.quitLabel = bitmapTextCentered(280, 'engeexpa', 'Quit', 30);
-
-    this.pauseKeyUp.onDown.add(this.moveCursorUp, this);
-    this.pauseKeyDown.onDown.add(this.moveCursorDown, this);
-    this.pauseKeyEnter.onDown.add(this.executeMenuOption, this);
-  },
-
-  quitPauseMenu: function() {
-    this.mask.destroy();
-    this.menuBg.destroy();
-    this.continueLabel.destroy();
-    this.restartLabel.destroy();
-    this.quitLabel.destroy();
-    this.cursor.destroy();
-
-    this.pauseKeyUp.onDown.removeAll();
-    this.pauseKeyDown.onDown.removeAll();
-    this.pauseKeyEnter.onDown.removeAll();
-  },
-
-  moveCursorDown: function() {
-    this.menuOption += 1;
-    if (this.menuOption > 2) this.menuOption = 0;
-  },
-
-  moveCursorUp: function() {
-    this.menuOption -= 1;
-    if (this.menuOption < 0) this.menuOption = 2;
-  },
-
-  executeMenuOption: function() {
-    if (this.menuOption === 1) {
-      game.state.start('play');
-    } else if (this.menuOption === 2) {
-      game.state.start('menu');
-    }
-    this.togglePause();
-  }
 
   //render: function() {
   //  game.debug.body(this.player);
