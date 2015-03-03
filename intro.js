1d57f121b4564614de9975e77da8814c32b13183
satanas <wil.alejandro@gmail.com>
2015-03-02 22:49:19 -0300

    Fixed typos in intro

--- intro.js
+++ intro.js
@@ -4,8 +4,8 @@ var introState = {
   create: function() {
     bitmapTextCentered(150, uiFonts.TITLE, 'Planet earth has been infected for hundred of', 26);
     bitmapTextCentered(180, uiFonts.TITLE, 'viruses. To save the world you must capture', 26);
-    bitmapTextCentered(210, uiFonts.TITLE, 'them usin cryogenic capsules.', 26);
-    bitmapTextCentered(250, uiFonts.TITLE, 'Future of humanity is in your hands', 26);
+    bitmapTextCentered(210, uiFonts.TITLE, 'them using cryogenic capsules.', 26);
+    bitmapTextCentered(250, uiFonts.TITLE, 'Future of humanity is in your hands...', 26);
     bitmapTextCentered(450, uiFonts.TITLE, 'Press ENTER to continue', 18);
 
     var enterKey = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
