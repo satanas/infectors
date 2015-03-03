e9a420c6de7fcea0c23277249c18278eb8ad7272
satanas <wil.alejandro@gmail.com>
2015-03-03 18:52:56 -0300

    Fixed error in humanizeTime function

--- common.js
+++ common.js
@@ -15,7 +15,8 @@ var DIRECTION = {
 var uiFonts = {
   TITLE: 'engeexpa',
   HUD: 'zerothre',
-  RECORD: 'record'
+  RECORD: 'record',
+  INSTRUCTIONS: 'instructions'
 };
 
 function findCapsule(x, y) {
@@ -39,8 +40,10 @@ function _find(x, y, group) {
 }
 
 function humanizeTime(time) {
+  if (time === undefined || time === null) return "--";
+
   time = time / 1000;
-  var min = Math.ceil(time / 60).toString();
+  var min = Math.floor(time / 60).toString();
   var sec = Math.ceil(time % 60).toString();
   if (min.length === 1) min = "0" + min
   if (sec.length === 1) sec = "0" + sec
