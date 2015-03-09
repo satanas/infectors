93a2f22573f0e8f458f5bf625e842a92d5a88f44
satanas <wil.alejandro@gmail.com>
2015-03-08 23:11:17 -0300

    Added some improvements to audiopool

--- audiopool.js
+++ audiopool.js
@@ -3,6 +3,7 @@
 var AudioPool = function(keys) {
   this.keys = keys;
   this.sounds = []
+  this.index = 0;
 
   for(var i=0; i<this.keys.length; i++) {
     this.sounds.push(game.add.audio(this.keys[i]));
@@ -14,7 +15,15 @@ AudioPool.prototype.constructor = AudioPool;
 AudioPool.prototype.randomPlay = function(loop, volume) {
   var volume = (volume === undefined) ? 1.0 : volume
   var loop = (loop === undefined) ? false : loop
-  var index = Math.floor(Math.random() * this.keys.length);
+  this.index = Math.floor(Math.random() * this.keys.length);
 
-  this.sounds[index].play('', 0, 1, volume, loop);
+  this.sounds[this.index].play('', 0, 1, volume, loop);
+};
+
+AudioPool.prototype.stop = function() {
+  this.sounds[this.index].stop();
+};
+
+AudioPool.prototype.resume = function() {
+  this.sounds[this.index].play();
 };
