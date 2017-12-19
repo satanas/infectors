72db2fbfffa75226c590e638b11c9e8475a84398
satanas <wil.alejandro@gmail.com>
2017-12-18 17:11:01 -0800

    Centering and scaling correctly

--- boot.js
+++ boot.js
@@ -1,6 +1,13 @@
 'use strict';
 
 var bootState = {
+  init: function() {
+    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    game.scale.pageAlignHorizontally = true;
+    game.scale.pageAlignVertically = true;
+    game.scale.refresh();
+  },
+
   preload: function() {
     game.load.image('progressbar', 'assets/images/progressbar.png');
     game.load.bitmapFont('engeexpa', 'assets/fonts/engeexpa.png', 'assets/fonts/engeexpa.fnt');
